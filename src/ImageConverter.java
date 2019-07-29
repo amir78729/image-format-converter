b6b80e9a4e135af7c0eeb1230b1d731ad15189bf
Amirhossein <amir78729@aut.ac.ir>
2019-07-30 00:09:50 +0430

    Initial commit

---
+++ src/ImageConverter.java
@@ -0,0 +1,34 @@
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class ImageConverter {
+
+    /**
+     * Converts an image to another format
+     *
+     * @param inputImagePath Path of the source image
+     * @param outputImagePath Path of the destination image
+     * @param formatName the format to be converted to, one of: jpeg, png,
+     * bmp, wbmp, and gif
+     * @return true if successful, false otherwise
+     * @throws IOException if errors occur during writing
+     */
+    public static boolean convertFormat(String inputImagePath,
+                                        String outputImagePath, String formatName) throws IOException {
+        FileInputStream inputStream = new FileInputStream(inputImagePath);
+        FileOutputStream outputStream = new FileOutputStream(outputImagePath);
+
+        BufferedImage inputImage = ImageIO.read(inputStream);
+
+        boolean result = ImageIO.write(inputImage, formatName, outputStream);
+
+        outputStream.close();
+        inputStream.close();
+
+        return result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String inputImage = "D:/negar.jpg";
+        String oututImage = "D:/output.png";
+        String formatName = "PNG";
+        try {
+            boolean result = ImageConverter.convertFormat(inputImage,
+                    oututImage, formatName);
+            if (result) {
+                System.out.println("Image converted successfully.");
+            } else {
+                System.out.println("Could not convert image.");
+            }
+        } catch (IOException ex) {
+            System.out.println("Error during converting image.");
+            ex.printStackTrace();
+        }
+    }
+}
