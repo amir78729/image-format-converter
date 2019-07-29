7df1627644e824f8db6d704d015be4c5f879b1e0
Amirhossein <amir78729@aut.ac.ir>
2019-07-30 01:22:40 +0430

    trying to make the GUI readable:))

--- src/GUI.java
+++ src/GUI.java
@@ -1,22 +1,27 @@
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import javax.swing.*;
 
 
-public class GUI {
+public class GUI implements ActionListener {
 
     private File file;
+    private String inputImage;
+    private String outputImage;
+    private String formatName;
 
-    public GUI() {
+    public GUI(){
         final JFrame frame = new JFrame("Image Format Converter");
 
-        JButton btnFile = new JButton("Select an image file!");
-        btnFile.addActionListener(new ActionListener() {
+        JButton chooseYourFileButton = new JButton("browse");
+        chooseYourFileButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 final JFileChooser fileChooser = new JFileChooser();
-                int returnVal = fileChooser.showOpenDialog(frame);
-                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                int returnValue = fileChooser.showOpenDialog(frame);
+                if (returnValue == JFileChooser.APPROVE_OPTION) {
                     file = fileChooser.getSelectedFile();
                     //This is where a real application would open the file.
                     System.out.println("File: " + file.getName() + ".");
@@ -24,19 +29,62 @@ public class GUI {
                 } else {
                     System.out.println("Open command cancelled by user.");
                 }
-                System.out.println(returnVal);
+                System.out.println(returnValue);
             }
         });
 
-        frame.getContentPane().add(btnFile);
+        JRadioButton png = new JRadioButton(".png");
+        png.setMnemonic(KeyEvent.VK_B);
+        png.setActionCommand(".png");
+        png.setSelected(true);
+
+        JRadioButton jpeg = new JRadioButton(".jpeg");
+        jpeg.setMnemonic(KeyEvent.VK_B);
+        jpeg.setActionCommand(".jpeg");
+
+        JRadioButton bmp = new JRadioButton(".bmp");
+        bmp.setMnemonic(KeyEvent.VK_B);
+        bmp.setActionCommand(".bmp");
+
+        JRadioButton gif = new JRadioButton(".gif");
+        gif.setMnemonic(KeyEvent.VK_B);
+        gif.setActionCommand(".gif");
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(png);
+        group.add(jpeg);
+        group.add(bmp);
+        group.add(gif);
+
+        jpeg.addActionListener(this);
+        png.addActionListener(this);
+        bmp.addActionListener(this);
+        gif.addActionListener(this);
+
+
+        JPanel formatsPanel = new JPanel(new FlowLayout());
+        formatsPanel.add(png);
+        formatsPanel.add(jpeg);
+        formatsPanel.add(bmp);
+        formatsPanel.add(gif);
+
+        frame.add(formatsPanel, BorderLayout.CENTER);
+
+        frame.setLayout(new BorderLayout());
+        frame.add(chooseYourFileButton , BorderLayout.SOUTH);
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
-    public static void main(String[] args) throws Exception {
-        new GUI();
     }
+
+
+//    public static void main(String[] args) throws Exception {
+//        new GUI();
+//    }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,22 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-        String inputImage = "D:/negar.jpg";
-        String oututImage = "D:/output.png";
-        String formatName = "PNG";
-        try {
-            boolean result = ImageConverter.convertFormat(inputImage,
-                    oututImage, formatName);
-            if (result) {
-                System.out.println("Image converted successfully.");
-            } else {
-                System.out.println("Could not convert image.");
-            }
-        } catch (IOException ex) {
-            System.out.println("Error during converting image.");
-            ex.printStackTrace();
-        }
+//        String inputImage = "D:/negar.jpg";
+//        String outputImage = "D:/output.png";
+//        String formatName = "PNG";
+//        try {
+//            boolean result = ImageConverter.convertFormat(inputImage,
+//                    outputImage, formatName);
+//            if (result) {
+//                System.out.println("Image converted successfully.");
+//            } else {
+//                System.out.println("Could not convert image.");
+//            }
+//        } catch (IOException ex) {
+//            System.out.println("Error during converting image.");
+//            ex.printStackTrace();
+//        }
+
+        new GUI();
     }
 }
