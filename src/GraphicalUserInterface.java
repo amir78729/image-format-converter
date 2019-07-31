0aff3484fe31ed7e69b016a76c4c045143158915
Amirhossein <amir78729@aut.ac.ir>
2019-07-31 17:09:23 +0430

    description

--- src/GraphicalUserInterface.java
+++ src/GraphicalUserInterface.java
@@ -7,6 +7,14 @@ import java.awt.event.MouseEvent;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * A java program that can convert an image format to another format...
+ * First we should select our file by clicking on the "browse" button.
+ * Then we can select the format that we are going to convert our image into.
+ * After selcting our file and the format, the "convert" button is going to be
+ * enable, so we can convert our image:)
+ * The converted image is going to be saved at the same path of our first file.
+ */
 public class GraphicalUserInterface extends JFrame implements ActionListener{
     private File file;
     private String inputImage = "C:\\Users\\Amirhossein A\\Desktop\\^_^.jpg";
